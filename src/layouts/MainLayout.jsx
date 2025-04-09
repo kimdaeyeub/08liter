ccc5e63cb180cc3e1b4828dc9e27dc9c138d5ef9
kimdaeyeub <kdy3915@naver.com>
2025-04-09 15:49:31 +0900

    Footer 컴포넌트 추가 및 MainLayout에 footer추가

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
 import GNB from "../components/Nav/GNB";
+import Footer from "../components/footer/footer";
 
 const MainLayout = () => {
   return (
     <div>
       <GNB />
       <Outlet />
+      <Footer />
     </div>
   );
 };
